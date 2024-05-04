9a2075fd99bec1a84801a5c79f2a266558901590
Imran Khan <imran.4.khan@gm.com>
2024-05-04 23:34:22 +0530

    May 4th Assignment

--- src/Assignments/May_4th/Strings/Anagram.java
+++ src/Assignments/May_4th/Strings/Anagram.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 public class Anagram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first string");
+        System.out.println("Enter first string:");
         String s1 = sc.nextLine();
-        System.out.println("Enter second string");
+        s1=s1.toLowerCase();
+        System.out.println("Enter second string:");
         String s2 = sc.nextLine();
+        s2=s2.toLowerCase();
 
         char[] arrayS1 = s1.toCharArray();
         char[] arrayS2 = s2.toCharArray();
