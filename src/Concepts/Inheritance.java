1bc0e4091f521635ea3b85bfd75cff2796d70397
Imran Khan <imran.4.khan@gm.com>
2024-05-08 05:26:09 +0530

    Concept - Inheritance

--- src/Concepts/Inheritance.java
+++ src/Concepts/Inheritance.java
@@ -28,11 +28,12 @@ public class Inheritance {
         B obj=new B();
         obj.test(); // Method overriding - When same method is present in parent and child -> child function is called
         obj.testB();
+        System.out.println("Inheriting parent function from child");
         obj.testA();
         System.out.println("Using super to call methods from parent using child object");
         obj.super_test();
 
-        System.out.println();
+        System.out.println("\nParent class object can only use its own member functions and not child's");
         A obj_A=new A();
         obj_A.test(); // parent Test method
         obj_A.test();
