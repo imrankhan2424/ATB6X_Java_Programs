e3c71a8c78b4fd884e542056fba9d7e8b908c4f0
Imran Khan <imran.4.khan@gm.com>
2024-05-08 05:32:02 +0530

    Concept - MethodOveridding

---
+++ src/Concepts/MethodOveridding.java
@@ -0,0 +1,11 @@
+package Concepts;
+
+public class MethodOveridding {
+    public static void main(String[] args) {
+        System.out.println("Method overriding - When same method is present in parent and child -> child function is called");
+        B obj=new B();
+        obj.test();
+    }
+}
+
+
