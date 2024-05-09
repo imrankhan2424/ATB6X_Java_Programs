382e48b5fe99d3b76081e72689b0ebf2076fef0a
Imran Khan <imran.4.khan@gm.com>
2024-05-09 05:58:03 +0530

    Concept

--- src/Concepts/MethodOveridding.java
+++ src/Concepts/MethodOveridding.java
@@ -1,6 +1,6 @@
 package Concepts;
 
-import Concepts.OOPS.B;
+import Concepts.OOPS.Inheritance_and_Polymorphism.B;
 public class MethodOveridding {
     public static void main(String[] args) {
         System.out.println("Method overriding - When same method is present in parent and child -> child function is called");
